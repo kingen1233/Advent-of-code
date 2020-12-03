3c1e8252b8bf3e853f2d02f9a185446216aca050
kingen1233 <andremattias.reis@gmail.com>
2020-12-03 20:53:08 +0100

    tidying done

--- 3/3b.js
+++ 3/3b.js
@@ -1,21 +1,24 @@
 const fs = require('fs')
 const data = fs.readFileSync('input.txt', 'utf8').split('\n')
 
+const slopes = [ 
+    [1, 1],
+    [3, 1],
+    [5, 1],
+    [7, 1],
+    [1, 2]
+]
 const result = []
 let finalValue = 1;
-result.push(countTreePath(1,1))
-result.push(countTreePath(3,1))
-result.push(countTreePath(5,1))
-result.push(countTreePath(7,1))
-result.push(countTreePath(1,2))
+
 //.forEach instead of for(i) just to annoy Oliver ;)
+slopes.forEach(i => result.push(countTreePath(i[0], i[1])))
 result.forEach(e => {finalValue = finalValue*e})
 console.log(finalValue)
 
 function countTreePath(rightStep, downStep) {
     const lineLength = 31;
-    let index = 0;
-    let count = 0;
+    let [index, count] = [0, 0]
     for (let i = downStep; i < data.length; i+=downStep) {
         index = (index + rightStep) % lineLength;
         const hit = data[i].charAt(index)
