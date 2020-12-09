1aec5f6ac9f2e907a1cc3d36c2cd5797f44c3a9b
kingen1233 <andremattias.reis@gmail.com>
2020-12-09 09:27:19 +0100

    9a more efficient with map

--- 9/9a.js
+++ 9/9a.js
@@ -4,14 +4,26 @@ const preamble = 25
 let found = false
 let invalidNumber = 0
 
+
+let indexMap = new Map
+let dupeMap = new Map
+
+for (let i = 0; i < data.length; i++) {
+    if (indexMap.has(parseInt(data[i]))) {
+        dupeMap.set(parseInt(data[i]), i)
+    }
+    else {
+        indexMap.set(parseInt(data[i]), i)
+    }
+}
 console.time("test")
 
-for(let i = preamble; i < data.length; i++){
+for (let i = preamble; i < data.length; i++) {
     invalidNumber = findImpossibleSum(i)
 
-    if(!found){
+    if (!found) {
         break;
-    }else{
+    } else {
         found = false;
     }
 }
@@ -19,19 +31,23 @@ for(let i = preamble; i < data.length; i++){
 // note to self -Mappa value -> index
 console.timeEnd("test")
 console.log('the invalid number is', invalidNumber)
-    
+
 
 // For one number from the data array, checks if any other two numbers, from index (nextIndex-25) -> nextIndex, adds up to this number
 function findImpossibleSum(nextIndex) {
     const sum = data[nextIndex]
-    const set = getValidNumbers(nextIndex)
-    for (let i = nextIndex - preamble; i < nextIndex; i++) {
+    let startIndex = nextIndex - preamble
+    for (let i = startIndex; i < nextIndex; i++) {
         const first = data[i]
         let second = sum - first
+        let indexOk2 = false
+        if(dupeMap.has(second)) indexOk2 = getIndexSpan(dupeMap.get(second), startIndex, nextIndex)
+
+        let indexOk = getIndexSpan(indexMap.get(second), startIndex, nextIndex)
         if (first == second || second < 0) {
             continue
         }
-        else if (set.has(second)) {
+        else if (indexMap.has(second) && indexOk || indexOk2) {
             found = true
             return
         }
@@ -41,11 +57,10 @@ function findImpossibleSum(nextIndex) {
     }
 
 }
- 
-function getValidNumbers(index) {
-    const set = new Set()
-    for (let i = index - preamble; i < index; i++) {
-        set.add(parseInt(data[i]))
+
+function getIndexSpan(index, startIndex, nextIndex) {
+    if (index > startIndex && index < nextIndex) {
+        return true
     }
-    return set
+    return false
 }
