63f4a94ca47eef582d4651772c663dde3e61e8be
kingen1233 <andremattias.reis@gmail.com>
2020-12-09 08:57:48 +0100

    9a made faster

--- 9/9a.js
+++ 9/9a.js
@@ -4,40 +4,53 @@ const preamble = 25
 let found = false
 let invalidNumber = 0
 
+console.time("test")
+
+const set = new Set()
+for(const d of data){
+    set.add(parseInt(d))
+}
 
 for(let i = preamble; i < data.length; i++){
     invalidNumber = findImpossibleSum(i)
 
     if(!found){
         break;
+    }else{
+        found = false;
     }
-    found = false;
 }
 
+// note to self -Mappa value -> index
+console.timeEnd("test")
 console.log('the invalid number is', invalidNumber)
-
-
     
 
 // For one number from the data array, checks if any other two numbers, from index (nextIndex-25) -> nextIndex, adds up to this number
 function findImpossibleSum(nextIndex) {
     const sum = data[nextIndex]
+    const set = getValidNumbers(nextIndex)
     for (let i = nextIndex - preamble; i < nextIndex; i++) {
         const first = data[i]
-        if (!found) {
-            for (let j = nextIndex - preamble; j < nextIndex; j++) {
-                const second = data[j]
-                if (first == second) {
-                }
-                else if (parseInt(second) + parseInt(first) == sum) {
-                    found = true;
-                    break;
-                }
-            }
+        let second = sum - first
+        if (first == second || second < 0) {
+            continue
+        }
+        else if (set.has(second)) {
+            found = true
+            return
         }
     }
     if (!found) {
         return sum
     }
 
+}
+ 
+function getValidNumbers(index) {
+    const set = new Set()
+    for (let i = index - preamble; i < index; i++) {
+        set.add(parseInt(data[i]))
+    }
+    return set
 }
